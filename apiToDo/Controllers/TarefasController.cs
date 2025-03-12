3176728f80690ef7e093b042278c76955c8cfab3
omegasenior777 <cesarhnrq02@gmail.com>
2025-03-11 21:23:57 -0300

    feat: criação de rotas de busca e atualização, modificação das rotas anteriores

--- apiToDo/Controllers/TarefasController.cs
+++ apiToDo/Controllers/TarefasController.cs
@@ -3,58 +3,119 @@ using apiToDo.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 
 namespace apiToDo.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    // Inserido versionamento da API
+    [Route("v1/[controller]")]
     public class TarefasController : ControllerBase
     {
-        [Authorize]
-        [HttpPost("lstTarefas")]
-        public ActionResult lstTarefas()
+        
+        //[Authorize]
+        // Remoção de definição endpoints por boa prática
+        [HttpGet]
+        // Melhoria da nomeclatura e definição de lista
+        public ActionResult<List<TarefaDTO>> ListarTarefas()
         {
             try
             {
-              
-                return StatusCode(200);
+                // Retone as tarefas em uma lista no formato indicado no Model Tarefas
+                return Ok(Tarefas.tarefas);
             }
-
             catch (Exception ex)
             {
-                return StatusCode(400, new { msg = $"Ocorreu um erro em sua API {ex.Message}"});
+                return BadRequest(new { msg = $"Erro: {ex.Message}" });
             }
         }
 
-        [HttpPost("InserirTarefas")]
-        public ActionResult InserirTarefas([FromBody] TarefaDTO Request)
+        // Rota para pegar um item da lista pelo Id
+        [HttpGet("{id}")]
+        public ActionResult ListarTarefa(int id)
         {
             try
-            {
+            {            
+                // Identifica a tarefa pelo Id rececbido
+                var tarefa = Tarefas.tarefas.Find(x => x.ID_TAREFA == id);
 
-                return StatusCode(200);
+                // Se o Id não existir retorna a mensagem de "Tarefa não encontrada."
+                if (tarefa == null) return NotFound(new { msg = "Tarefa não encontrada." });
 
-
+                return Ok(tarefa);
             }
-
             catch (Exception ex)
             {
-                return StatusCode(400, new { msg = $"Ocorreu um erro em sua API {ex.Message}" });
+                return BadRequest(new { msg = $"Erro: {ex.Message}" });
             }
         }
 
-        [HttpGet("DeletarTarefa")]
-        public ActionResult DeleteTask([FromQuery] int ID_TAREFA)
+        [HttpPost]
+        public ActionResult InserirTarefa([FromBody] TarefaDTO request)
         {
             try
             {
+                // Transforma a Id das tarefas para incremental
+                request.ID_TAREFA = Tarefas.tarefas.Count + 1;
 
-                return StatusCode(200);
+                //Adiciona a tarefa a lista
+                Tarefas.tarefas.Add(request);
+
+                // Retorna HTTP 201 Created com a nova tarefa
+                return CreatedAtAction(nameof(ListarTarefa), new { id = request.ID_TAREFA },  request );
             }
-
             catch (Exception ex)
             {
-                return StatusCode(400, new { msg = $"Ocorreu um erro em sua API {ex.Message}" });
+                return BadRequest(new { msg = $"Erro: {ex.Message}" });
+            }
+        }
+
+        // Rota para atualizar um item da lista pelo Id
+        [HttpPatch]
+        public ActionResult AtualizarTarefa([FromBody] TarefaDTO dto)
+        {
+            try
+            {
+                // Identifica a tarefa pelo Id rececbido
+                var tarefa = Tarefas.tarefas.Find(x => x.ID_TAREFA == dto.ID_TAREFA);
+
+                // Se o Id não existir retorna a mensagem de "Tarefa não encontrada."
+                if (tarefa == null) return NotFound(new { msg = "Tarefa não encontrada." });
+
+                // Modifica a descrição da tarefa, se fornecida
+                if (!string.IsNullOrEmpty(dto.DS_TAREFA))
+                {
+                    tarefa.ID_TAREFA = dto.ID_TAREFA;
+                    tarefa.DS_TAREFA = dto.DS_TAREFA;
+                }
+                // Retorna lista de tarefas
+                return Ok(Tarefas.tarefas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { msg = $"Erro: {ex.Message}" });
+            }
+        }
+
+        // Usando método DELETE
+        [HttpDelete]
+        public ActionResult DeletarTarefa([FromQuery] int id)
+        {
+            try
+            {
+                // Identifica a tarefa pelo Id rececbido
+                var tarefa = Tarefas.tarefas.Find(x => x.ID_TAREFA == id);
+
+                // Se o Id não existir retorna a mensagem de "Tarefa não encontrada."
+                if (tarefa == null) return NotFound(new { msg = "Tarefa não encontrada." });
+
+                // Se existir a tarefa é removida de lista
+                Tarefas.tarefas.Remove(tarefa);
+                return Ok(new { msg = "Tarefa removida com sucesso!" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { msg = $"Erro: {ex.Message}" });
             }
         }
     }
